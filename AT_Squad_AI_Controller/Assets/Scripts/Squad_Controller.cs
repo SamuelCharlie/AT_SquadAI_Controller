2a0a41017f198006047ba31df6fe02e4ad8c79d9
Samuel Tamkin <scharlie.tamkin@gmail.com>
2022-10-11 11:45:19 +0100

    Added Unity Project to Repository

---
+++ AT_Squad_AI_Controller/Assets/Scripts/Squad_Controller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squad_Controller : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    public GameObject player;
+    void Update()
+    {
+        
+    }
+}
